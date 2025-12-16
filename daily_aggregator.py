eb53e5203ddf4abdfe1d529b6546dc904caf709f
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2025-12-16 08:32:22 +0800

    Create daily_aggregator.py

---
+++ daily_aggregator.py
@@ -0,0 +1,8 @@
+  Python2_ROOT_DIR: /opt/hostedtoolcache/Python/3.11.14/x64
+  Python3_ROOT_DIR: /opt/hostedtoolcache/Python/3.11.14/x64
+  LD_LIBRARY_PATH: /opt/hostedtoolcache/Python/3.11.14/x64/lib
+  File "/home/runner/work/Artikelkolektor/Artikelkolektor/artikel.py", line 3
+    ✔ Jalan harian (queue-based, GitHub Actions friendly) ✔ Ambil N artikel per hari ✔ Output HTML GABUNGAN seperti script LAMA parts/chord_part1.html, chord_part2.html, dst
+    ^
+SyntaxError: invalid character '✔' (U+2714)
+Process completed with exit code 1.
